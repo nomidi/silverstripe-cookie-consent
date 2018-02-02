5d3af88cc617e722b09f7c97a78773df84a9bf28
ArminEden <eden@bit-basti.de>
2018-02-02 15:30:34 +0100

    js files will no be included via load instead of inline

--- code/CookieConsent_Extension.php
+++ code/CookieConsent_Extension.php
@@ -13,8 +13,9 @@ class CookieConsent_Extension extends Extension
                 Requirements::css('//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css');
             } else {
                 // Catch files from template and parse inline!
-                Requirements::insertHeadTags($siteConfig->customise(array('SiteConfig'=>$siteConfig))->renderWith('CookieConsent_min_js'));
+                //Requirements::insertHeadTags($siteConfig->customise(array('SiteConfig'=>$siteConfig))->renderWith('CookieConsent_min_js'));
                 Requirements::insertHeadTags($siteConfig->customise(array('SiteConfig'=>$siteConfig))->renderWith('CookieConsent_min_css'));
+                Requirements::javascript(COOKIE_CONSENT_PATH.'/thirdparty/cookieconsent/build/cookieconsent.min.js');
             }
             Requirements::insertHeadTags($siteConfig->customise(array('SiteConfig'=>$siteConfig))->renderWith('CookieConsent_js'));
         }
